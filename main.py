08a059d51f1c2ee8948bb050374e979bd5f937dc
amalkrishna0 <amalkrishna528@gmail.com>
2025-02-05 10:42:37 +0530

    Storing to a file

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ from auth import authenticate_spotify
 from function import get_liked_songs
 
 def main():
-    sp = authenticate_spotify()  # Authenticate with Spotify
+    sp = authenticate_spotify()  
     print("Fetching Liked Songs...")
 
-    liked_songs = get_liked_songs(sp)
+    get_liked_songs(sp)
 
-    print("\nYour Liked Songs:")
-    for song in liked_songs:
-        print(song)
 
-    print(f"\nTotal Liked Songs: {len(liked_songs)}")
 
 if __name__ == "__main__":
     main()
